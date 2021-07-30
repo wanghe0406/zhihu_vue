11f09c46d5452865efc4ca7e48ca181806d9766a
Lough <1730790915@qq.com>
2021-07-30 15:32:31 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-07-21 08:17:25
+ * @LastEditTime: 2021-07-27 14:02:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue\src\App.vue
+-->
+<template>
+  <div>
+    <div>
+ <!--      <router-link to="/item"></router-link> -->
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style>
+
+</style>
