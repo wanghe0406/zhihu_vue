a72e4da1ecf7ff25a81f9247de7f385785e7935c
Lough <1730790915@qq.com>
2021-08-02 13:13:44 +0800

    master

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-07-21 08:17:25
- * @LastEditTime: 2021-07-30 16:20:10
+ * @LastEditTime: 2021-08-02 13:13:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \vue\src\App.vue
@@ -9,7 +9,7 @@
 <template>
   <div>
     <div>
- <!--      <router-link to="/item"></router-link> -->
+ <!--      <router-link11 to="/item"></router-link11> -->
     </div>
     <router-view/>
   </div>
